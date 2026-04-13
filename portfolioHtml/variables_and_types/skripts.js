3cc1b44423f199e58c698d1ffe4f096ed90732fc
Joona Pesonen <joona.pesonen@kukkula.local>
2026-04-13 12:17:17 +0300

    Muuttujia ja tietotyyppejä sivu tehty ja sen js

---
+++ portfolioHtml/variables_and_types/skripts.js
@@ -0,0 +1,17 @@
+var a = 3;
+var b = 5;
+var c = 7;
+var d = 15;
+
+function summa(){
+    alert(`${a} + ${b} = ${a + b}`)
+}
+function vahennys(){
+    alert(`${b} - ${c} = ${b - c}`)
+}
+function kerto(){
+    alert(`${c} * ${d} = ${c * d}`)
+}
+function jako(){
+    alert(`${d} / ${a} = ${d / a}`)
+}
