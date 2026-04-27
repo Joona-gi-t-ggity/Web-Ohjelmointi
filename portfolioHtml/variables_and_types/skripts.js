128ab030b72b4474ae1f73946497f353f9cf4b20
Joona Pesonen <joona.pesonen@kukkula.local>
2026-04-27 14:07:32 +0300

    lisäsin datetime jutut variables and types tehtävän juttuhihin nyt näkyy kuukaus ja päviä ja aiak ja muuu :D

--- portfolioHtml/variables_and_types/skripts.js
+++ portfolioHtml/variables_and_types/skripts.js
@@ -35,3 +35,30 @@ function laskin_jako() {
 
     jako(num1, num2)
 }
+
+const weekdayArray = ["Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai"]
+const monthArray = ["Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kesäkuu", "Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"]
+
+function datetimeLoad() {
+    let dateObj = new Date();
+
+    const date = dateObj.toLocaleDateString();
+    const time = dateObj.toLocaleTimeString();
+
+    const datetime = `${date}, klo: ${time}`;
+
+    const weekday = dateObj.getDay();
+    const month = dateObj.getMonth();
+
+    var dateDiv = document.getElementById("date");
+    var timeDiv = document.getElementById("time");
+    var datetimeDiv = document.getElementById("datetime");
+    var weekdayDiv = document.getElementById("weekday");
+    var monthDiv = document.getElementById("month");
+
+    dateDiv.innerHTML = `Päivämäärä = ${date}`;
+    timeDiv.innerHTML = `Aika = ${time}`;
+    datetimeDiv.innerHTML = `Päivä ja aika = ${datetime}`;
+    weekdayDiv.innerHTML = `Vikkonpäivä = ${weekdayArray[weekday]}`;
+    monthDiv.innerHTML = `Kuukausi = ${monthArray[month]}`;
+}
